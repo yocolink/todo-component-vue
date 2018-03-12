7e184832fa0b36132175deea755df2a671cd54a4
Ludovic Nitoumbi <blanchardludovic.nitoumbi@1000mercis.com>
2018-03-12 19:12:27 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    TODO MAISON
+    <todo/>
+    ------
+    TODO TRAVAIL
+    <todo/>
+    ------
+    TODO FETE
+    <todo/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import todo from './components/todo'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="todo-list">
+    <input v-model="todoText" placeholder="Input TODO"/>
+    <button @click="add()">Add</button>
+    <button @click="removeAll()">Delete all</button>
+
+    <div v-for="(item, index) in items" :key="index">
+      <span @click="done=!done">{{index}} - {{item.text}}</span>
+      <button @click="remove(index)">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      items: [],
+      todoText: ''
+    }
+  },
+  methods: {
+    add () {
+      this.items.push({text: this.todoText})
+      this.todoText = ''
+    },
+    remove (index) {
+      this.items.splice(index, 1)
+    },
+    removeAll () {
+      this.items = []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
